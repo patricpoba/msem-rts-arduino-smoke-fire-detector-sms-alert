4abc63669b2526abb2ffd94b8d951ad94ca5eafd
Patric Poba <poba.dev@outlook.com>
2020-12-26 14:17:10 +0100

    Add LCD display

--- smoke_detection_with_buzzer.ino
+++ smoke_detection_with_buzzer.ino
@@ -1,3 +1,8 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int redLed = 12;
 int greenLed = 11;
@@ -20,32 +25,61 @@ void setup() {
   pinMode(flamePin, INPUT);
   
   Serial.begin(9600);
+
+  // initialize the LCD and turn on the blacklight.
+  lcd.begin(); 
+  lcd.backlight();
+  lcd.print("Starting ..."); 
+}
+
+
+void printOnLcd(String text, int line = 1){
+  
+  // line numbering starts with zero
+  line = line -1 ; 
+  lcd.setCursor(0,line);
+
+  // print 16 spaces to clear existing text on specified line
+  lcd.print("                "); 
+
+  lcd.setCursor(0,line);
+  lcd.print( text );
+
+  // for debugging purposes, remove befor final presentation
+  Serial.println(text);
 }
 
 void loop() {
   
   checkForGasOrSmoke();
 
-//  delay(100);
+  // tone(buzzer, 1000, 200);
+  delay(500);
  
   checkForFlames();
 }
 
 void checkForGasOrSmoke(){
-  
+//  tone(buzzer, 1000, 200);
   int analogSensor = analogRead(smokeA0);
 
-  Serial.print("Pin A0: ");
-  Serial.println(analogSensor);
+//  Serial.print("Pin A0: ");
+//  Serial.println(analogSensor);
+  String lcdText = "Gas/Smoke: ";
+  printOnLcd(lcdText += analogSensor);
+  
   // Checks if it has reached the threshold value
   if (analogSensor > smokeSensorThreshold)
   {
+    printOnLcd("SMOKE DECTECTED!", 2);
+    
     digitalWrite(redLed, HIGH);
     digitalWrite(greenLed, LOW);
     tone(buzzer, 1000, 200);
   }
   else
   {
+    printOnLcd("                ", 2);
     digitalWrite(redLed, LOW);
     digitalWrite(greenLed, HIGH);
     noTone(buzzer);

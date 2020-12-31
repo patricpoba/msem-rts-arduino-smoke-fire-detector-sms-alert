74ea584c646a75532d28bbdf5055b42505b371ba
Patric Poba <poba.dev@outlook.com>
2020-12-31 01:06:03 +0100

    add sms alert

--- smoke_detection_with_buzzer.ino
+++ smoke_detection_with_buzzer.ino
@@ -1,8 +1,13 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//Create software serial object to communicate with SIM800L
+SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+
 
 int redLed = 12;
 int greenLed = 11;
@@ -13,6 +18,7 @@ int smokeSensorThreshold = 400;
 
 const int flamePin = 9;
 int Flame = LOW;
+
 
 void setup() {
   
@@ -30,8 +36,13 @@ void setup() {
   lcd.begin(); 
   lcd.backlight();
   lcd.print("Starting ..."); 
+  
+  //Begin serial communication with Arduino and SIM800L
+  mySerial.begin(9600);
+  
   delay(1000);
 }
+
 
 void clearLcdScreen(int line = 1){
   // line numbering starts with zero 
@@ -40,6 +51,7 @@ void clearLcdScreen(int line = 1){
   // print 16 spaces to clear existing text on specified line
   lcd.print("                "); 
 }
+
 
 void printOnLcd(String text, int line = 1){
 
@@ -54,19 +66,58 @@ void printOnLcd(String text, int line = 1){
   Serial.println(text);
 }
 
-void sendAlert(String fireOrSmoke){
-  String alertMessage = "SENDING SMS alert for  . . . ";
-  Serial.println( alertMessage += fireOrSmoke);
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
 }
+
+
+void sendAlert(String fireOrSmoke){
+  String alertMessage = " detected in your house!";
+  alertMessage =  fireOrSmoke + alertMessage;
+  Serial.println(alertMessage);
+
+  // send sms
+  Serial.println("Initializing..."); 
+  delay(1000);
+
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  mySerial.println("AT+CMGS=\"+491638267508\"");//change 49 with country code and xxxxxxxxxxx with phone number to sms
+  updateSerial();
+  mySerial.print( alertMessage ); //text content
+  updateSerial();
+  mySerial.write(26);
+
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+
+ //  AT+COPS? – Check that you’re connected to the network, in this case BSNL 
+ //  AT+COPS=? – Return the list of operators present in the network
+}
+
 
 void loop() {
   
   checkForGasOrSmoke();
  
-//  delay(500);
+  delay(1000);
  
   checkForFlames();
 }
+
 
 void checkForGasOrSmoke(){
 //  tone(buzzer, 1000, 200);
@@ -94,6 +145,7 @@ void checkForGasOrSmoke(){
     noTone(buzzer);
   }
 }
+
 
 void checkForFlames(){
   int flameSensorReading = analogRead(A0);
